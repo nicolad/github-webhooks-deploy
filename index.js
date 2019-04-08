312bc5ea49c787fd63395e32bf397bbfc43170f4
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-04-08 20:39:46 +0300

    Updated.

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ const http = require("http");
 const crypto = require("crypto");
 const exec = require("child_process").exec;
 
+const hostname = "localhost";
+const port = 3000;
 const repo = "~/your_repo_path_here/";
 
-http
-  .createServer(function(req, res) {
-    req.on("data", function(chunk) {
-      console.log("process.env.SECRET", process.env.SECRET);
+const server = http.createServer((req, res) => {
+  req.on("data", function(chunk) {
+    console.log("process.env.SECRET", process.env.SECRET);
 
-      const sig =
-        "sha1=" +
-        crypto
-          .createHmac("sha1", process.env.SECRET)
-          .update(chunk.toString())
-          .digest("hex");
+    const sig =
+      "sha1=" +
+      crypto
+        .createHmac("sha1", process.env.SECRET)
+        .update(chunk.toString())
+        .digest("hex");
 
-      if (req.headers["x-hub-signature"] == sig) {
-        exec("cd " + repo + " && git pull");
-      }
-    });
+    if (req.headers["x-hub-signature"] == sig) {
+      exec("cd " + repo + " && git pull");
+    }
+  });
 
-    res.end();
-  })
-  .listen(8080);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
