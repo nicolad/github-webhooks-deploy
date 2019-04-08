88d0d1d29bf4f6f3aa18b627ac65a0b08792a416
Vadim Nicolai <nicolai.vadim@gmail.com>
2019-04-08 22:06:52 +0300

    Added res.end()

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const server = http.createServer((req, res) => {
 
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
+  res.end();
 });
 
 server.listen(port, hostname, () => {
